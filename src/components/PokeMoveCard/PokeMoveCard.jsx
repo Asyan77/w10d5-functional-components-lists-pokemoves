4d0e8230da8b81aa6ed541848142d40118415a0e
Ashley <asyan77@gmail.com>
2023-08-06 17:40:20 -0700

    finish project

---
+++ src/components/PokeMoveCard/PokeMoveCard.jsx
@@ -0,0 +1,15 @@
+import "./PokeMovesCard.css"
+
+const PokeMoveCard = ({type, id, level, move}) => {
+    return (
+        <li className='poke-move-card'>
+            <h3>Move: {id}</h3>
+            <h4 style={{padding: 10}}>{move.toUpperCase()}</h4>
+            <p>Type: {type}</p>
+            <p>Level: {level}</p>
+        </li>
+        )
+
+}
+
+export default PokeMoveCard
